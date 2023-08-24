35a6fa4cdb9c15c5c5903b6964cd74503f690a21
Lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-08-24 21:14:24 +0500

    ahha🖐

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const {Telegraf} = require("telegraf");
-const {
-    handleRegistration,
-    findContact,
-    chatContact,
-    getUser,
-    connect,
-    config
-} = require('./controller/base.js');
-const { handleFindingPartner } = require('./controller/findPartner.js');
-const { handleMessage } = require('./controller/isCmd.js');
-const {handleSession} = require("./controller/skip.js");
-const {handleStopSession} = require("./controller/stop.js");
-const sendBroadcastMessage = require("./controller/bc.js");
+// const {
+//     handleRegistration,
+//     findContact,
+//     chatContact,
+//     getUser,
+//     connect,
+//     config
+// } = require('./controller/base.js');
+// const { handleFindingPartner } = require('./controller/findPartner.js');
+// const { handleMessage } = require('./controller/isCmd.js');
+// const {handleSession} = require("./controller/skip.js");
+// const {handleStopSession} = require("./controller/stop.js");
+// const sendBroadcastMessage = require("./controller/bc.js");
 const { config: cnf } = require('dotenv');
 const express = require("express");
 const colors = require('colors')
@@ -22,76 +22,78 @@ app.use(express.json());
 cnf();
 
 const bot = new Telegraf(process.env.botToken);
-connect().then(()=>{});
+// connect().then(()=>{});
 
 
 bot.on('message', async (userScope) => {
 
-    const body = userScope.update.message.text || userScope.message.caption || userScope.message.text || ''
-    const command = body.split(' ')[0]
-    const isCmd = body.startsWith('/')
-    // const isGroup = userScope.chat.type.includes("group")
-    const from = userScope.chat.id
+    userScope.sendMessage("hey salom")
+
+    // const body = userScope.update.message.text || userScope.message.caption || userScope.message.text || ''
+    // const command = body.split(' ')[0]
+    // const isCmd = body.startsWith('/')
+    // // const isGroup = userScope.chat.type.includes("group")
+    // const from = userScope.chat.id
 
     // console.log(userScope.chat)
     // Database Query
 
-    const sender = getUser(userScope.message.from)
+    // const sender = getUser(userScope.message.from)
     // Get user ID
     // const userid = sender.id
     // Get user name
-    const username = sender.username
+    // const username = sender.username
     // Owner, change in config.json
-    const isOwner = config.ownerUsername.includes(username)
+    // const isOwner = config.ownerUsername.includes(username)
 
     // Media Type
-    const isText = userScope.message.hasOwnProperty("text")
-    const isImage = userScope.message.hasOwnProperty("photo")
-    const isVideo = userScope.message.hasOwnProperty("video")
-    const isAudio = userScope.message.hasOwnProperty("audio")
-    const isSticker = userScope.message.hasOwnProperty("sticker")
-    const isContact = userScope.message.hasOwnProperty("contact")
-    // const isLocation = userScope.message.hasOwnProperty("location")
-    const isDocument = userScope.message.hasOwnProperty("document")
-    const isAnimation = userScope.message.hasOwnProperty("animation")
+    // const isText = userScope.message.hasOwnProperty("text")
+    // const isImage = userScope.message.hasOwnProperty("photo")
+    // const isVideo = userScope.message.hasOwnProperty("video")
+    // const isAudio = userScope.message.hasOwnProperty("audio")
+    // const isSticker = userScope.message.hasOwnProperty("sticker")
+    // const isContact = userScope.message.hasOwnProperty("contact")
+    // // const isLocation = userScope.message.hasOwnProperty("location")
+    // const isDocument = userScope.message.hasOwnProperty("document")
+    // const isAnimation = userScope.message.hasOwnProperty("animation")
 
-    if (!isCmd) {
+    // if (!isCmd) {
 
-        try {
-            await handleMessage(bot, userScope, isText, isSticker, isImage, isVideo, isAudio, isContact, isDocument, isAnimation, chatContact);
-        } catch (err) {
-            console.log(err);
+    //     try {
+    //         await handleMessage(bot, userScope, isText, isSticker, isImage, isVideo, isAudio, isContact, isDocument, isAnimation, chatContact);
+    //     } catch (err) {
+    //         console.log(err);
 
-            return bot.telegram.sendMessage(from, config.mess.error.notRegistered, {parse_mode: "Markdown"});
-        }
-    }
+    //         return bot.telegram.sendMessage(from, config.mess.error.notRegistered, {parse_mode: "Markdown"});
+    //     }
+    // }
 
-    switch (command) {
-        case "/register":
-            await handleRegistration(bot, from, userScope);
-            break;
-        case "/start":
-            await handleFindingPartner(from, bot, userScope);
-            break;
-        case "/unregister":
-            findContact(from).then(async (res) => {
-                if (res !== null) {
-                    await res.remove();
-                    await bot.telegram.sendMessage(from, config.mess.unRegisterSuccess, {parse_mode: "Markdown"});
-                }
-            });
-            break;
-        case "/next":
-            await handleSession(from, bot);
-            break;
-        case "/stop":
-            await handleStopSession(from, bot);
-            break;
-        case "/bc":
-        case "/broadcast":
-            await sendBroadcastMessage(bot, from, isOwner, isImage, userScope, body, command);
-            break;
-    }
+    // switch (command) {
+    //     case "/register":
+    //         await handleRegistration(bot, from, userScope);
+    //         break;
+    //     case "/start":
+    //         await handleFindingPartner(from, bot, userScope);
+    //         break;
+    //     case "/unregister":
+    //         findContact(from).then(async (res) => {
+    //             if (res !== null) {
+    //                 await res.remove();
+    //                 await bot.telegram.sendMessage(from, config.mess.unRegisterSuccess, {parse_mode: "Markdown"});
+    //             }
+    //         });
+    //         break;
+    //     case "/next":
+    //         await handleSession(from, bot);
+    //         break;
+    //     case "/stop":
+    //         await handleStopSession(from, bot);
+    //         break;
+    //     case "/bc":
+    //     case "/broadcast":
+    //         await sendBroadcastMessage(bot, from, isOwner, isImage, userScope, body, command);
+    //         break;
+    // }
 })
 
 app.get('/', (req,res) => {
